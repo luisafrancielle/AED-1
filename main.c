d8d6f3b4e39e8c9ddc33b4ddf75dc07b433dea71
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-01-31 01:22:25 -0300

    Organização do projeto e declaração de algumas variáveis ( 'i' e 'j' )

--- main.c
+++ main.c
@@ -2,60 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "metodos.h"
 
 clock_t start, end;
 double time_spent;
 
-typedef struct {
-    int id;
-    char nome[20];
-    char profissao[50];
-    int idade;
-} Pessoa;
-
-// Função de ordenação InsertionSort pelo id
-void insertionSort(Pessoa pessoas[], int cont) {
-int i, j;
-
 Pessoa chave;
-for (i = 1; i < cont; i++) {
+for (int i = 1; i < cont; i++) {
 chave = pessoas[i];
-j = i - 1;
+int j = i - 1;
    while (j >= 0 && pessoas[j].id > chave.id) {
         pessoas[j + 1] = pessoas[j];
         j = j - 1;
     }
     pessoas[j + 1] = chave;
-    }
-}
-
-// Função de ordenação SelectionSort pelo id
-void selectionSort(Pessoa pessoas[], int cont) {
-    int i, j, min_idx;
-    Pessoa temp;
-    for (i = 0; i < cont-1; i++) {
-        min_idx = i;
-        for (j = i+1; j < cont; j++)
-            if (pessoas[j].id < pessoas[min_idx].id)
-                min_idx = j;
-        temp = pessoas[min_idx];
-        pessoas[min_idx] = pessoas[i];
-        pessoas[i] = temp;
-    }
-}
-
-// Função de ordenação ShellSort pelo id
-void shellSort(Pessoa pessoas[], int cont) {
-    int i, j, gap;
-    Pessoa temp;
-    for (gap = cont/2; gap > 0; gap /= 2) {
-        for (i = gap; i < cont; i++) {
-            temp = pessoas[i];
-            for (j = i; j >= gap && pessoas[j - gap].id > temp.id; j -= gap) {
-                pessoas[j] = pessoas[j - gap];
-            }
-            pessoas[j] = temp;
-        }
     }
 }
 
